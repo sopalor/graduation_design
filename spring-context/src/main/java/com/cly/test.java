7e31630c6cce32cb0a2313ef95b6a0b9a0506ba3
cly <273641466@qq.com>
2020-06-10 16:27:37 +0800

    【*】迁移问题

---
+++ spring-context/src/main/java/com/cly/test.java
@@ -0,0 +1,11 @@
+package com.cly;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class test {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext
+				annotationConfigApplicationContext = new AnnotationConfigApplicationContext("com.cly");
+
+	}
+}
